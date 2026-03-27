b318f08b3677f3662a6bd9e1d89eee935d01da20
rob587 <robertocamm02@yahoo.com>
2026-03-27 12:07:03 +0100

    Remove unused tab layout and screens, and update index component with new styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>ciao</Text>
+    <View style={styles.contenitore}>
+      <View style={styles.box}>
+        <Text>ciao</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  contenitore: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  box: {
+    height: 0,
+    width: 150,
+    borderRadius: 20,
+    backgroundColor: "#2a2a2a",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
