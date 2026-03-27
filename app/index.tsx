df8092ce86c8bd66aa703fe6d0c28713d041948e
rob587 <robertocamm02@yahoo.com>
2026-03-27 12:13:08 +0100

    Update index component to display styled circles in the box

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,9 @@ export default function Index() {
   return (
     <View style={styles.contenitore}>
       <View style={styles.box}>
-        <Text>ciao</Text>
+        <Text style={styles.circles}></Text>
+        <Text style={styles.circle2}></Text>
+        <Text style={styles.circle3}></Text>
       </View>
     </View>
   );
@@ -18,12 +20,48 @@ const styles = StyleSheet.create({
   },
 
   box: {
-    height: 0,
-    width: 150,
+    height: 20,
+    width: 100,
     borderRadius: 20,
     backgroundColor: "#2a2a2a",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  circles: {
+    borderRadius: 28,
+    borderStyle: "solid",
+
+    borderWidth: 2, // ← senza questo il bordo non appare
+    backgroundColor: "red", // ← senza questo è trasparente
+    height: 56,
+    width: 56,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+  },
+  circle2: {
+    borderRadius: 28,
+    borderStyle: "solid",
+    borderWidth: 2, // ← senza questo il bordo non appare
+    backgroundColor: "grey", // ← senza questo è trasparente
+    height: 56,
+    width: 56,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+  },
+  circle3: {
+    borderRadius: 28,
+    borderStyle: "solid",
+
+    borderWidth: 2, // ← senza questo il bordo non appare
+    backgroundColor: "grey", // ← senza questo è trasparente
+    height: 56,
+    width: 56,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
   },
 });
